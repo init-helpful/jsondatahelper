6f8eb5faaba1995405fbb6f405a382488bd5575b
cummingsa <57905451+cummingsa@users.noreply.github.com>
2021-04-23 20:08:21 -0400

    Fixed small indexing issue in subpath_value function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jsondatahelper",
-    version="0.0.6",
+    version="0.0.7",
     description="Collection of utility functions for JSON data manipulation.",
     py_modules=['jsondatahelper'],package_dir={"":"src"}
 )
